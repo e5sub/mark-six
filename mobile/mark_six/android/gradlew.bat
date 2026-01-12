8babf1c38299b4fc38b83c1941aa7e22d46b7fae
菜芽 <46080660+e5sub@users.noreply.github.com>
2026-01-12 11:53:35 +0800

    fix  bug

--- mobile/mark_six/android/gradlew.bat
+++ mobile/mark_six/android/gradlew.bat
@@ -2,7 +2,7 @@
 SETLOCAL
 
 SET APP_HOME=%~dp0
-SET CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar;%APP_HOME%gradle\wrapper\gradle-wrapper-shared-8.0.2.jar
+SET CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar;%APP_HOME%gradle\wrapper\gradle-wrapper-shared-8.0.2.jar;%APP_HOME%gradle\wrapper\gradle-cli-8.0.2.jar
 
 IF DEFINED JAVA_HOME (
   SET JAVA_CMD=%JAVA_HOME%\bin\java.exe
